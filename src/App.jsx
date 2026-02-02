b79fa6c5d01d85fec228a282a7447716ea7ca64b
Vishalnaidu77 <vminorr@gmail.com>
2026-02-02 18:15:29 +0530

    Create Navbar and DateTime function

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import "./app.scss"
 import Dock from './Components/Dock'
+import Nav from './Components/Nav'
 
 const App = () => {
   return (
     <main>
+      <Nav />
       <Dock />
     </main>
   )
---
+++ src/Components/DateTime.jsx
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from 'react'
+
+const DateTime = () => {
+  const [dateTime, setDateTime] = useState(new Date())
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setDateTime(new Date())
+    }, 1000)
+
+    return () => clearInterval(timer)
+  }, [])
+
+  const formatDateTime = (date) => {
+    const days = ['sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat']
+    const months = ['jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'oct', 'nov', 'dec']
+    
+    const dayName = days[date.getDay()]
+    const monthName = months[date.getMonth()]
+    const dayOfMonth = date.getDate()
+    
+    let hours = date.getHours()
+    const minutes = date.getMinutes()
+    const ampm = hours >= 12 ? 'PM' : 'AM'
+    
+    hours = hours % 12
+    hours = hours ? hours : 12 // 0 should be 12
+    
+    const minutesStr = minutes < 10 ? '0' + minutes : minutes
+    
+    return `${dayName} ${monthName} ${dayOfMonth} ${hours}:${minutesStr}${ampm}`
+  }
+
+  return (
+    <div>
+      {formatDateTime(dateTime)}
+    </div>
+  )
+}
+
+export default DateTime
