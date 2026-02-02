733a9815799600390a10bb35f5818697beabca6c
Vishalnaidu77 <vminorr@gmail.com>
2026-02-02 18:40:26 +0530

    Make Draggable component using RND

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,16 @@ import React from 'react'
 import "./app.scss"
 import Dock from './Components/Dock'
 import Nav from './Components/Nav'
+import MacWindow from './Components/Windows/MacWindow'
 
 const App = () => {
   return (
     <main>
       <Nav />
       <Dock />
+      <MacWindow>
+        <h1>hello</h1>
+      </MacWindow>
     </main>
   )
 }
---
+++ src/Components/Windows/MacWindow.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Rnd } from "react-rnd"
+import "../Windows/window.scss"
+
+const MacWindow = ({ children }) => {
+  return (
+    <Rnd>
+        <div className="window">
+            <div className="nav">
+                <div className="dots">
+                    <div className="dot red"></div>
+                    <div className="dot yellow"></div>
+                    <div className="dot green"></div>
+                </div>
+
+                <div className="title">
+                    <p>vishalnaidu - zsh</p>
+                </div>
+            </div>
+            <div className="main-component">
+                {children}
+            </div>
+        </div>
+    </Rnd>
+  )
+}
+
+export default MacWindow
