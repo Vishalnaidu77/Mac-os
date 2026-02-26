4aadce84170e886e79fcc95f0b8ef09f35502e5d
Vishal naidu <vminorr@gmail.com>
2026-02-26 03:54:44 +0000

    fix: update iframe source and scrolling behavior in Calendar component

--- src/Components/Windows/Calender.jsx
+++ src/Components/Windows/Calender.jsx
@@ -7,9 +7,9 @@ const Calender = ({ windowName}) => {
     <MacWindow windowName={windowName}>
         <div className="calender-window">
             <iframe 
-            src="https://calendar.google.com/calendar/embed?height=600&wkst=1&ctz=UTC&showPrint=0&src=ZGV2ZWxvcG1lbnQuZ2FyZ2llbnRlcnByaXNlc0BnbWFpbC5jb20&src=ZW4uaW5kaWFuI2hvbGlkYXlAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&color=%23039be5&color=%230b8043" 
-            width="100%" height="100%"frameBorder="0" scrolling="yes"
-            ></iframe>
+            src="https://calendar.google.com/calendar/embed?height=600&wkst=1&ctz=UTC&showPrint=0&src=dm1pbm9yckBnbWFpbC5jb20&src=ZW4uaW5kaWFuI2hvbGlkYXlAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&color=%23039be5&color=%230b8043" 
+            style={{border:"solid 1px #777"}} width="100%" height="100%" frameborder="0" scrolling="no">
+            </iframe>
         </div>
     </MacWindow>
   )
